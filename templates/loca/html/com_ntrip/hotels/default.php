40c49b4868747b93fac16b7b1166474b0158b502
Viet Trang <viettrang@viettrang-desktop.(none)>
2013-03-27 23:39:05 +0700

    css for rating star

--- templates/loca/html/com_ntrip/hotels/default.php
+++ templates/loca/html/com_ntrip/hotels/default.php
@@ -4,7 +4,81 @@ defined('_JEXEC') or die;
 
 $fields = $this->fields;
 ?>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script>
+	// This is the first thing we add ------------------------------------------
+	$(document).ready(function() {
 
+		$('.rate_widget').each(function(i) {
+			var widget = this;
+			var out_data = {
+				widget_id : $(widget).attr('id'),
+				fetch: 1
+			};
+			$.post(
+				'ratings.php',
+				out_data,
+				function(INFO) {
+					$(widget).data( 'fsr', INFO );
+					set_votes(widget);
+				},
+				'json'
+			);
+		});
+
+
+		$('.ratings_stars').hover(
+			// Handles the mouseover
+			function() {
+				$(this).prevAll().andSelf().addClass('ratings_over');
+				$(this).nextAll().removeClass('ratings_vote');
+			},
+			// Handles the mouseout
+			function() {
+				$(this).prevAll().andSelf().removeClass('ratings_over');
+				// can't use 'this' because it wont contain the updated data
+				set_votes($(this).parent());
+			}
+		);
+
+
+		// This actually records the vote
+		$('.ratings_stars').bind('click', function() {
+			var star = this;
+			var widget = $(this).parent();
+
+			var clicked_data = {
+				clicked_on : $(star).attr('class'),
+				widget_id : $(star).parent().attr('id')
+			};
+			$.post(
+				'ratings.php',
+				clicked_data,
+				function(INFO) {
+					widget.data( 'fsr', INFO );
+					set_votes(widget);
+				},
+				'json'
+			);
+		});
+
+
+
+	});
+
+	function set_votes(widget) {
+		var avg = $(widget).data('fsr').whole_avg;
+		var votes = $(widget).data('fsr').number_votes;
+		var exact = $(widget).data('fsr').dec_avg;
+
+		window.console && console.log('and now in set_votes, it thinks the fsr is ' + $(widget).data('fsr').number_votes);
+
+		$(widget).find('.star_' + avg).prevAll().andSelf().addClass('ratings_vote');
+		$(widget).find('.star_' + avg).nextAll().removeClass('ratings_vote');
+		$(widget).find('.total_votes').text( votes + ' votes recorded (' + exact + ' rating)' );
+	}
+	// END FIRST THING
+</script>
 <div id="top-adv">
 		<img src="<?php echo JURI::base() . 'templates/loca/images/top-adv.jpg'; ?>" />
 	</div>
@@ -135,7 +209,11 @@ $fields = $this->fields;
 						</div>
 
 						<div class="rating-content">
-							<img src="<?php echo JURI::base() . 'templates/loca/'; ?>images/rate.gif" />
+							<div class="star_1 ratings_stars"></div>
+							<div class="star_2 ratings_stars"></div>
+							<div class="star_3 ratings_stars"></div>
+							<div class="star_4 ratings_stars"></div>
+							<div class="star_5 ratings_stars"></div>
 							234 lượt đánh giá
 						</div>
 						<a class="promotion-link" href="#">KHuyến mại đặt 2 tặng 1 chỉ có tai nhà hàng Hạ Long</a>
