aa526d579cf8be0dd53f84eb3df50f9111443205
Nguyen <Nguyen@192.168.23.70>
2013-04-01 10:52:23 +0700

    add table user_like

--- db/customize.sql
+++ db/customize.sql
@@ -11,3 +11,15 @@ ALTER TABLE  `jos_ntrip_services` ADD  `user_like` FLOAT( 11, 2 ) NOT NULL AFTER
 ALTER TABLE  `jos_ntrip_shoppings` ADD  `user_like` FLOAT( 11, 2 ) NOT NULL AFTER  `alias`;
 ALTER TABLE  `jos_ntrip_tours` ADD  `user_like` FLOAT( 11, 2 ) NOT NULL AFTER  `alias`;
 ALTER TABLE  `jos_ntrip_comments` ADD  `user_like` FLOAT( 11, 2 ) NOT NULL AFTER  `rating`;
+
+-- 01/04/2012 - add table user like
+CREATE TABLE `jos_ntrip_user_like` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) DEFAULT NULL,
+  `item_type` varchar(255) DEFAULT NULL,
+  `like` float(11,2) DEFAULT NULL COMMENT '1',
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
