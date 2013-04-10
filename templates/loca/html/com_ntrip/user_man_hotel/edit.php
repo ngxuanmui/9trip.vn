14c66c869ef55fe59b2c5bf02defb6904bf48dc9
Viet Trang <viettrang@viettrang-desktop.(none)>
2013-04-11 00:06:34 +0700

    10.04.2013

--- templates/loca/html/com_ntrip/user_man_hotel/edit.php
+++ templates/loca/html/com_ntrip/user_man_hotel/edit.php
@@ -28,6 +28,128 @@ $jqueryFileUploadPath = JURI::root() . 'media/jquery-ui-upload/';
 	}
 </script>
 
+<div id="top-adv">
+	<img src="<?php echo JURI::base() . 'templates/loca/images/top-adv.jpg'; ?>" />
+</div>
+<div class="clear"></div>
+
+<form action="<?php echo JRoute::_('index.php?option=com_ntrip&task=user_man_hotel.apply&id='.(int) $this->item->id); ?>" method="post" name="userForm" id="hotel-form" class="form-validate" enctype="multipart/form-data">
+<div id="left-content">
+	<div class="container-edit-page">
+		<h1>Thêm mới khách sạn</h1>
+		<div class="saparate-line-breakcrum"></div>
+		<div style="padding: 10px">
+			<div>
+				<label class="title-field">Địa chỉ</label>
+				<span class="fltrt note">(Tối đa 250 ký tự)</span>
+			</div>
+			<?php echo $this->form->getInput('address'); ?>
+
+			<div>
+				<label class="title-field">Tên khách sạn</label>
+				<span class="fltrt note">(Tối đa 250 ký tự)</span>
+			</div>
+			<?php echo $this->form->getInput('name'); ?>
+
+			<div class="col">
+				<label class="title-field">Dịch vụ</label>
+				<div class="clear"></div>
+				<?php echo $this->form->getInput('type'); ?>
+			</div>
+			<div class="clear"></div>
+
+			<div class="col">
+				<label class="title-field">Tỉnh thành</label>
+				<?php echo $this->form->getInput('catid'); ?>
+			</div>
+			
+			<div class="clear"></div>
+
+			<div class="fltlft col">
+				<label class="title-field">Website</label>
+				<div><?php echo $this->form->getInput('website'); ?></div>
+			</div>
+			<div class="fltlft col">
+				<label class="title-field">Email liên hệ</label>
+				<div><?php echo $this->form->getInput('email'); ?></div>
+			</div>
+			<div class="clear"></div>
+
+			<div class="fltlft col">
+				<label class="title-field">Điện thoại</label>
+				<div><?php echo $this->form->getInput('hotline'); ?></div>
+			</div>
+			<div class="clear"></div>
+
+			<div>
+				<label class="title-field">Mô tả</label>
+				<span class="fltrt note">(Tối đa 250 ký tự)</span>
+			</div>
+			<div class="clear"><?php echo $this->form->getInput('description'); ?></div>
+
+			<div class="fltlft col">
+				<label class="title-field">Giá từ</label>
+				<div><?php echo $this->form->getInput('price_from'); ?></div>
+			</div>
+			<div class="fltlft col">
+				<label class="title-field">Đến</label>
+				<div><?php echo $this->form->getInput('price_to'); ?></div>
+			</div>
+			<div class="clear"></div>
+
+			<div class="fltlft col">
+				<label class="title-field">Trạng thái</label>
+				<div><?php echo $this->form->getInput('state'); ?></div>
+			</div>
+			<div class="clear"></div>
+
+			<div class="clear">
+				<label class="title-field">Cơ sở vật chất</label>
+			</div>
+			<div class="clear">
+				<?php echo $this->form->getInput('images'); ?>
+				<div class="fltright">
+					<a href="#" class="icons loca-button"><span class="txt-btn">Thêm ảnh</span></a>
+				</div>
+			</div>
+
+			<?php
+				$introImages = ($this->item->images) ? $this->item->images : false;
+			?>
+
+			<?php if ($introImages): ?>
+			<div class="fltlft">
+				<label>Xóa ảnh</label>
+				<?php echo $this->form->getInput('del_image'); ?>
+			</div>
+			<label>Intro image uploaded</label>
+			<a href="<?php echo JUri::root() . $introImages; ?>" class="modal">
+				<img src="<?php echo JUri::root() . $introImages; ?>" style="width: 100px;" />
+			</a>
+			<?php endif; ?>
+			<div class="clear">
+				<div class="fltlft" style="margin-right: 10px;"><a href="#" class="icons loca-button"><span class="txt-btn">LƯU THÔNG TIN</span></a></div>
+				<div class="fltlft"><a href="#" class="icons loca-button"><span class="txt-btn">TẠO KHÁCH SẠN MỚI</span></a></div>
+			</div>
+		</div>
+
+		<div class="clear"></div>
+	</div>
+</div>
+</form>
+
+<div id="right-content">
+	<a class="register" href="<?php echo JRoute::_('index.php?option=com_users&view=registration', false); ?>" style="display: block;">
+		<span class="icon-reg"></span>
+		<span class="txt-register">ĐĂNG KÝ THÀNH VIÊN</span>
+	</a>
+	
+	<?php echo LocaHelper::renderModulesOnPosition('right'); ?>
+
+</div>
+<div class="clear"></div>
+
+
 <form action="<?php echo JRoute::_('index.php?option=com_ntrip&task=user_man_hotel.apply&id='.(int) $this->item->id); ?>" method="post" name="userForm" id="hotel-form" class="form-validate" enctype="multipart/form-data">
 	<div class="width-60 fltlft">
 		<fieldset class="adminform">
