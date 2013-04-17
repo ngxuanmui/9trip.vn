b66d29e991c6ad80f1acc31b7455e6c2f9ad8435
Nguyen Xuan Mui <nguyenxuanmui@gmail.com>
2013-04-17 13:59:51 +0700

    Update temp

--- plugins/system/location/location.php
+++ plugins/system/location/location.php
@@ -31,7 +31,7 @@ class plgSystemLocation extends JPlugin
 	{
 		$get = JRequest::get('get');	
 		
-		if ($get['option'] == 'com_ntrip' && isset($get['view']) && $get['view'] == 'category' && $get['id'] > 0)
+		if (isset($get['option']) && $get['option'] == 'com_ntrip' && isset($get['view']) && $get['view'] == 'category' && $get['id'] > 0)
 			JFactory::getSession()->set('loca_location', $get['id']);			
 	}
 }
