f7f66a1bd77502b01d14ed4d7dc7318f64ef0a31
Ng Xuan Mui <nguyenxuanmui@gmail.com>
2013-11-27 00:06:52 -0800

    loca.vn: Update backend.

--- loca/includes.php
+++ loca/includes.php
@@ -3,4 +3,4 @@
 require_once 'defines.php';
 require_once 'loca.helper.php';
 require_once JPATH_ROOT . '/components/com_ntrip/helpers/route.php';
-require_once JPATH_ROOT.'/loca/facebooksdk/facebook.php';
+// require_once JPATH_ROOT.'/loca/facebooksdk/facebook.php';
